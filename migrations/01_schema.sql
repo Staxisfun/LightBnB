6495fdaa6a15906010328c2566d4020e4f0c9423
Matt Wilson <mwilson82@hotmail.com>
2022-10-20 00:01:32 +0000

    added 4 new tables to lightbnb project

---
+++ migrations/01_schema.sql
@@ -0,0 +1,62 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+
+
+
+
+
+CREATE TABLE users (
+id SERIAL PRIMARY KEY NOT NULL,
+name VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL
+);
+
+
+
+
+
+CREATE TABLE properties (
+id SERIAL PRIMARY KEY NOT NULL,
+title VARCHAR(255) NOT NULL,
+description TEXT,
+cost_per_night INT NOT NULL DEFAULT 0,
+parking_spaces INT NOT NULL DEFAULT 0,
+number_bathrooms INT NOT NULL DEFAULT 0,
+number_bedrooms INT NOT NULL DEFAULT 0,
+thumbnail_photo VARCHAR(255) NOT NULL,
+cover_photo VARCHAR(255) NOT NULL,
+country VARCHAR(255) NOT NULL,
+street VARCHAR(55) NOT NULL,
+city VARCHAR(55) NOT NULL,
+province VARCHAR(55) NOT NULL,
+post_code VARCHAR(255) NOT NULL,
+active BOOLEAN NOT NULL DEFAULT TRUE,
+owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+
+
+
+CREATE TABLE reservations (
+id SERIAL PRIMARY KEY NOT NULL,
+start_date DATE NOT NULL,
+end_date DATE NOT NULL,
+guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+);
+
+
+
+
+CREATE TABLE property_reviews (
+id SERIAL PRIMARY KEY NOT NULL,
+message TEXT,
+rating SMALLINT NOT NULL DEFAULT 0,
+guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE
+);
